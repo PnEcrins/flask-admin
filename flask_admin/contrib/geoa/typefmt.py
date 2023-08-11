bcc015d51afc256bdfffdb5f19a99046e1f2969d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-08-11 18:03:17 +0200

    change formatter

--- flask_admin/contrib/geoa/typefmt.py
+++ flask_admin/contrib/geoa/typefmt.py
@@ -1,28 +1,16 @@
 from flask_admin.contrib.sqla.typefmt import DEFAULT_FORMATTERS as BASE_FORMATTERS
-from markupsafe import Markup
-from wtforms.widgets import html_params
 from geoalchemy2.shape import to_shape
 from geoalchemy2.elements import WKBElement
-from sqlalchemy import func
+from shapely import to_geojson
 
 
 def geom_formatter(view, value):
-    params = html_params(**{
-        "data-role": "leaflet",
-        "disabled": "disabled",
-        "data-width": 100,
-        "data-height": 70,
-        "data-geometry-type": to_shape(value).geom_type,
-        "data-zoom": 15,
-        "data-tile-layer-url": view.tile_layer_url,
-        "data-tile-layer-attribution": view.tile_layer_attribution
-    })
-
+    # TODO check srid utility
     if value.srid == -1:
         value.srid = 4326
 
-    geojson = view.session.query(view.model).with_entities(func.ST_AsGeoJSON(value)).scalar()
-    return Markup('<textarea %s>%s</textarea>' % (params, geojson))
+    r = to_geojson(to_shape(value))
+    return to_geojson(to_shape(value))
 
 
 DEFAULT_FORMATTERS = BASE_FORMATTERS.copy()
